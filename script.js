7ead8b1e3192690b9aca48912223ef5815827dce
Alex iMac Casa <aartiles@aidacanarias.com>
2025-07-06 10:55:54 +0100

    Commit inicial

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("¡Página cargada y lista!");
+    document.getElementById('app').textContent = '¡Hola Mundo desde JavaScript!';
+});
