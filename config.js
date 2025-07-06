ba20f69689cba1dedb2fd33ccfd67234189f66fa
Alex iMac Casa <aartiles@aidacanarias.com>
2025-07-06 12:41:08 +0100

    Permitir que config.js se suba a GitHub para GitHub Pages

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBg6XG7-nh2B4iMgbez4DgcvdJtyPWEuU",
+  authDomain: "lo-compro-aartiles.firebaseapp.com",
+  databaseURL: "https://lo-compro-aartiles-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lo-compro-aartiles",
+  storageBucket: "lo-compro-aartiles.firebasestorage.app",
+  messagingSenderId: "441056570869",
+  appId: "1:441056570869:web:e0f09ec574fa9bf33604bc"
+};
