2be474b5b762f360b48a8fd8543f414517002c87
Alex iMac Casa <aartiles@aidacanarias.com>
2025-07-12 11:09:27 +0100

    chore: Actualizar hash de commit en la UI

--- script.js
+++ script.js
@@ -440,7 +440,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Mostrar el hash del commit
-    const commitHash = 'dc6b55b'; // ESTE VALOR SE ACTUALIZARÁ AUTOMÁTICAMENTE EN CADA COMMIT
+    const commitHash = '86e0406'; // ESTE VALOR SE ACTUALIZARÁ AUTOMÁTICAMENTE EN CADA COMMIT
     const commitHashDisplay = document.getElementById('commit-hash-display');
     if (commitHashDisplay) {
         commitHashDisplay.textContent = `v: ${commitHash}`;
